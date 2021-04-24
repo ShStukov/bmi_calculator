ac7a6a927db6532a89ac67b04f43d4e8c8604500
Alexandr <227stukov1337@gmail.com>
2021-04-24 14:23:01 +0300

    bmi_calculator
    bmi_calculator

---
+++ lib/components/round_icon_button.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class RoundIconButton extends StatelessWidget {
+  RoundIconButton({@required this.icon, @required this.onPressed});
+
+  final IconData icon;
+  final Function onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return RawMaterialButton(
+      child: Icon(icon),
+      onPressed: onPressed,
+      elevation: 6.0,
+      constraints: BoxConstraints.tightFor(
+        width: 56.0,
+        height: 56.0,
+      ),
+      shape: CircleBorder(),
+      fillColor: Color(0xFF0E3B50),
+    );
+  }
+}
+
+class RoundIconButtonHeight extends StatelessWidget {
+  RoundIconButtonHeight({@required this.icon, @required this.onPressed});
+
+  final IconData icon;
+  final Function onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return RawMaterialButton(
+      child: Icon(icon),
+      onPressed: onPressed,
+      elevation: 6.0,
+      constraints: BoxConstraints.tightFor(
+        width: 35.0,
+        height: 35.0,
+      ),
+      shape: CircleBorder(),
+      fillColor: Color(0xFF0E3B50),
+    );
+  }
+}
---
+++ lib/constants.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+const kBottomContainerHeight = 80.0;
+const kActiveCardColour = Color(0xFF8DC3CC);
+const kInactiveCardColour = Color(0xFF63AAB5);
+const kBottomContainerColour = Color(0xFFF5D8A7);
+
+const kLabelTextStyle = TextStyle(
+  fontSize: 18.0,
+  color: Color(0xFF0E3B50),
+);
+
+const kNumberTextStyle = TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.w900,
+);
+
+const kLargeButtonTextStyle = TextStyle(
+  fontSize: 25.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kTytleTextStyle = TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kResultsTextStyle = TextStyle(
+  color: Color(0xFF1E4041),
+  fontSize: 22.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kBMITextStyle = TextStyle(fontSize: 100.0, fontWeight: FontWeight.bold);
+
+const kBodyTextStyle = TextStyle(
+  fontSize: 22.0,
+);
